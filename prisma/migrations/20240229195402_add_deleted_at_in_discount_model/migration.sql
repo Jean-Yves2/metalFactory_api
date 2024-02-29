2f4c5c5e382de4a78e3d8788748b424f076d8f2f
Yunaluman THERESE <therese_yun@outlook.fr>
2024-02-29 22:34:41 +0100

    feat(api): add discount crud
    * add deltedAt in discount model
    
    * add create discount dto
    
    * add update discount dto
    
    * add discount controller
    
    * add discount service
    
    * add discount controller test
    
    * add discount service test
    
    * add discount migration
    
    * update prisma

---
+++ prisma/migrations/20240229195402_add_deleted_at_in_discount_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Discount" ADD COLUMN     "deleted_at" TIMESTAMP(3);
--- src/discount/discount.service.ts
+++ src/discount/discount.service.ts
@@ -1,4 +1,78 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateDiscountDto } from './dto/create-discount.dto';
+import { UpdateDiscountDto } from './dto/update-discount.dto';
+import { PrismaService } from '../database/prisma/prisma.service';
+import { Discount } from '@prisma/client';
 
 @Injectable()
-export class DiscountService {}
+export class DiscountService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getAllDiscounts(): Promise<Discount[]> {
+    return this.prismaService.discount.findMany({
+      where: { deletedAt: null },
+    });
+  }
+
+  async getDiscountById(id: number): Promise<Discount> {
+    const discount = await this.prismaService.discount.findUnique({
+      where: { id, deletedAt: null },
+    });
+    if (!discount) {
+      throw new NotFoundException(`Discount with ID ${id} not found`);
+    }
+    return discount;
+  }
+
+  async createDiscount(
+    createDiscountDto: CreateDiscountDto,
+  ): Promise<Discount> {
+    return this.prismaService.discount.create({
+      data: {
+        ...createDiscountDto,
+        createdAt: new Date(),
+        quotes: { create: createDiscountDto.quotes }, // Add this line to fix the type error
+      },
+    });
+  }
+
+  async updateDiscount(
+    id: number,
+    updateDiscountDto: UpdateDiscountDto,
+  ): Promise<Discount> {
+    const discount = await this.prismaService.discount.findUnique({
+      where: { id, deletedAt: null },
+    });
+    if (!discount) {
+      throw new NotFoundException(`Discount with ID ${id} not found`);
+    }
+    return this.prismaService.discount.update({
+      where: { id },
+      data: {
+        ...updateDiscountDto,
+        updatedAt: new Date(),
+        quotes: {
+          updateMany: {
+            where: { discountId: id },
+            data: updateDiscountDto.quotes,
+          },
+        }, // Update the quotes property to use the 'updateMany' operation with the missing 'where' and 'data' properties
+      },
+    });
+  }
+
+  async softDelete(id: number): Promise<Discount> {
+    const discount = await this.prismaService.discount.findUnique({
+      where: { id, deletedAt: null },
+    });
+    if (!discount) {
+      throw new NotFoundException(`Discount with ID ${id} not found`);
+    }
+    return this.prismaService.discount.update({
+      where: { id },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+  }
+}
