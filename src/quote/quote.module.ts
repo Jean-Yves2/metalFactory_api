9830b788d0199505aa613e4b59b84bf2d6f1b7d2
Yunaluman THERESE <therese_yun@outlook.fr>
2024-03-10 19:18:24 +0100

    feat(api): add quote crud
    * add quote crud
    * add .env.default file
    * add migration for quote model
    * add create and update quote dto
    * add quote controller
    * add quote service
    * add quote module
    * add quote controller test
    * add quote service test

--- src/quote/quote.module.ts
+++ src/quote/quote.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QuoteService } from './quote.service';
 import { QuoteController } from './quote.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [QuoteService],
   controllers: [QuoteController],
 })
