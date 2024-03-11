961cff040236feec8f037dcd292800752263a2be
cabrelyvans <dhscabrel@gmail.com>
2024-03-11 11:19:47 +0100

    premier commit

---
+++ prisma/migrations/20240311095423_add_deleted_at_in_stock_model/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Stock" ADD COLUMN     "deleted_at" TIMESTAMP(3);
--- src/stock/stock.controller.spec.ts
+++ src/stock/stock.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StockController } from './stock.controller';
+import { StockService } from './stock.service';
+import { PrismaService } from '../database/prisma/prisma.service';
 
 describe('StockController', () => {
   let controller: StockController;
@@ -7,6 +9,7 @@ describe('StockController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StockController],
+      providers: [StockService, PrismaService],
     }).compile();
 
     controller = module.get<StockController>(StockController);
