3cad980f4f96e6a3e7477250f415aa4314fe9977
Jean-Yves2 <sarakajean1@gmail.com>
2024-03-12 15:03:59 +0100

    style : add space

--- src/user/dto/createUserdto.ts
+++ src/user/dto/createUserdto.ts
@@ -1,24 +1,30 @@
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  
   @IsNotEmpty()
   @IsString()
   firstName: string;
+
   @IsNotEmpty()
   @IsString()
   lastName: string;
 
   @IsString()
   email: string;
+
   @IsString()
   @IsOptional()
   password: string;
+
   @IsNotEmpty()
   @IsString()
   companyName: string;
+
   @IsString()
   @IsOptional()
   address: string;
+
   @IsNotEmpty()
   @IsString()
   phone: string;
