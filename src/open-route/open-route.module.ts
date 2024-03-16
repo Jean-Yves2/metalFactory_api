483596a8fe18bd31e09ef1e5273a15fca1bae157
Yunaluman THERESE <therese_yun@outlook.fr>
2024-03-16 21:02:51 +0100

    feat(api): Add HttpModule and dependencies to OpenRouteModule

--- src/open-route/open-route.module.ts
+++ src/open-route/open-route.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { OpenRouteService } from './open-route.service';
 import { OpenRouteController } from './open-route.controller';
 
 @Module({
+  imports: [HttpModule],
   providers: [OpenRouteService],
-  controllers: [OpenRouteController]
+  controllers: [OpenRouteController],
 })
 export class OpenRouteModule {}
--- src/open-route/open-route.service.spec.ts
+++ src/open-route/open-route.service.spec.ts
@@ -1,15 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OpenRouteService } from './open-route.service';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { of } from 'rxjs';
 
 describe('OpenRouteService', () => {
   let service: OpenRouteService;
+  let httpService: HttpService;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OpenRouteService],
+      imports: [HttpModule],
+      providers: [
+        OpenRouteService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockImplementation((key: string) => {
+              if (key === 'OPEN_ROUTE_API_KEY') return 'test-api-key';
+              if (key === 'OPEN_ROUTE_API_URL') return 'test-api-url';
+            }),
+          },
+        },
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn().mockReturnValue(of({ data: {} })),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<OpenRouteService>(OpenRouteService);
+    httpService = module.get<HttpService>(HttpService);
+    configService = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
