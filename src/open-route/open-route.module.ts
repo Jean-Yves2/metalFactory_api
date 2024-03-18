7a2629ebade90a6951ba2c51ec7b415d31d7eaf6
Yunaluman THERESE <therese_yun@outlook.fr>
2024-03-18 22:25:30 +0100

    fix(api): Add ConfigModule to OpenRouteModule

--- src/open-route/open-route.module.ts
+++ src/open-route/open-route.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { OpenRouteService } from './open-route.service';
 import { OpenRouteController } from './open-route.controller';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule],
   providers: [OpenRouteService],
   controllers: [OpenRouteController],
 })
