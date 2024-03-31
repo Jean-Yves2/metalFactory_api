500c160316958fa9defc106d4921ef4bf9f1e23d
Yunaluman THERESE <therese_yun@outlook.fr>
2024-03-31 01:22:40 +0100

    feat(api):Refactor order-line and quote service error handling

--- src/quote/quote.service.ts
+++ src/quote/quote.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateQuoteDto } from './dto/create-quote.dto';
 import { UpdateQuoteDto } from './dto/update-quote.dto';
 import { PrismaService } from '../database/prisma/prisma.service';
@@ -9,63 +13,96 @@ export class QuoteService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getAllQuotes(): Promise<Quote[]> {
-    // Logic to fetch all quotes from a data source
-    return this.prismaService.quote.findMany({
-      where: { deletedAt: null },
-    });
+    try {
+      return await this.prismaService.quote.findMany({
+        where: { deletedAt: null },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Error fetching all quotes');
+    }
   }
 
   async getQuoteById(id: number): Promise<Quote> {
-    // Logic to fetch a quote by its ID from a data source
-    const quote = await this.prismaService.quote.findUnique({
-      where: { id, deletedAt: null },
-    });
-    if (!quote) {
-      throw new NotFoundException(`Quote with ID ${id} not found`);
+    try {
+      const quote = await this.prismaService.quote.findUnique({
+        where: { id, deletedAt: null },
+      });
+      if (!quote) {
+        throw new NotFoundException(`Quote with ID ${id} not found`);
+      }
+      return quote;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        `Error fetching quote with id ${id}`,
+      );
     }
-    return quote;
   }
 
   async createQuote(createQuoteDto: CreateQuoteDto): Promise<Quote> {
-    return this.prismaService.quote.create({
-      data: {
-        ...createQuoteDto,
-        createdAt: new Date(),
-      },
-    });
+    try {
+      return await this.prismaService.quote.create({
+        data: {
+          ...createQuoteDto,
+          createdAt: new Date(),
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Error creating quote');
+    }
   }
 
   async updateQuote(
     id: number,
     updateQuoteDto: UpdateQuoteDto,
   ): Promise<Quote> {
-    const quote = await this.prismaService.quote.findUnique({
-      where: { id, deletedAt: null },
-    });
-    if (!quote) {
-      throw new NotFoundException(`Quote with ID ${id} not found`);
+    try {
+      const quote = await this.prismaService.quote.findUnique({
+        where: { id, deletedAt: null },
+      });
+      if (!quote) {
+        throw new NotFoundException(`Quote with ID ${id} not found`);
+      }
+      return await this.prismaService.quote.update({
+        where: { id },
+        data: {
+          ...updateQuoteDto,
+          updatedAt: new Date(),
+        },
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        `Error updating quote with id ${id}`,
+      );
     }
-    return this.prismaService.quote.update({
-      where: { id },
-      data: {
-        ...updateQuoteDto,
-        updatedAt: new Date(),
-      },
-    });
   }
 
   async softDelete(id: number): Promise<Quote> {
-    const quote = await this.prismaService.quote.findUnique({
-      where: { id, deletedAt: null },
-    });
-    if (!quote) {
-      throw new NotFoundException(`Quote with ID ${id} not found`);
+    try {
+      const quote = await this.prismaService.quote.findUnique({
+        where: { id, deletedAt: null },
+      });
+      if (!quote) {
+        throw new NotFoundException(`Quote with ID ${id} not found`);
+      }
+      return await this.prismaService.quote.update({
+        where: { id },
+        data: {
+          deletedAt: new Date(),
+        },
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        `Error deleting quote with id ${id}`,
+      );
     }
-    return this.prismaService.quote.update({
-      where: { id },
-      data: {
-        deletedAt: new Date(),
-      },
-    });
   }
 }
