b94a3f8e54db7b10f4b1d5d700c46111d74f8e72
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-01 16:39:06 +0200

    feat : Refactor user service mock and add tests for user controller

--- src/user/mocks/user.service.mock.ts
+++ src/user/mocks/user.service.mock.ts
@@ -1,23 +1,38 @@
-export class UserServiceMock {
-  geatAllUsers = jest.fn().mockResolvedValue([
-    {
-      id: 1,
-      firstName: 'Ana',
-      lastName: "D'Amore",
-      email: 'Jamey55@gmail.com',
-      role: 'ADMIN',
-    },
-    {
-      id: 2,
-      firstName: 'Ansley',
-      lastName: 'Crona',
-      email: 'Miller.Crooks92@yahoo.com',
-      role: 'USER',
-    },
-  ]);
+import { userMock } from './user.mock';
+import { CreateUserDto } from '../dto/createUserdto';
 
-  getUserById = jest.fn();
-  updateUser = jest.fn();
+export class UserServiceMock {
+  getAllUsers = jest.fn().mockImplementation(() => {
+    return userMock.map((user) => {
+      return {
+        ...user,
+      };
+    });
+  });
+
+  getUserById = jest.fn().mockImplementation((id: number) => {
+    return userMock.find((user) => user.id === id);
+  });
+
+  updateUser = jest
+    .fn()
+    .mockImplementation((id: number, createUserDto: CreateUserDto) => {
+      const userToUpdate = userMock.find((user) => user.id === id);
+
+      if (!userToUpdate) {
+        return null;
+      }
+      const updatedUser = {
+        ...userToUpdate,
+        ...createUserDto,
+      };
+
+      if (createUserDto.address && createUserDto.address.type) {
+        updatedUser.address.type = createUserDto.address.type;
+      }
+
+      return updatedUser;
+    });
   softDelete = jest.fn();
   findUserByEmail = jest.fn();
 }
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -6,6 +6,8 @@ import { ConfigService } from '@nestjs/config';
 import { ConfigServiceMock } from './mocks/config.service.mock';
 import { JwtService } from '@nestjs/jwt';
 import { JwtServiceMock } from './mocks/jwt.service.mock';
+import { userMock } from './mocks/user.mock';
+import { CreateUserDto } from './dto/createUserdto';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -25,5 +27,56 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return an array of users', async () => {
+    expect(await controller.getAllUsers()).toEqual(userMock);
+  });
+
+  it('should return a user with specific id', async () => {
+    const id = 1;
+    const user = userMock.find((user) => user.id === id);
+    expect(await controller.getUserById(id)).toEqual(user);
+  });
+
+  it('should update a user with specific id', async () => {
+    const id = 1;
+    const createUserDto: CreateUserDto = {
+      firstName: 'Michael',
+      lastName: 'Smith deckard',
+      email: 'm.smith@example.com',
+      password: 'SecurePass123!',
+      companyName: 'Smith & Co.',
+      address: {
+        street: '456 Maple Avenue',
+        postalCode: '54321',
+        city: 'Mapletown',
+        country: 'United States',
+        distanceToWarehouse: 20,
+        type: 'DELIVERY',
+      },
+      phone: '987-654-3210',
+    };
+
+    const userToUpdate = userMock.find((user) => user.id === id);
+
+    if (!userToUpdate) {
+      throw new Error(`User with id ${id} not found`);
+    }
+
+    const updatedUser = {
+      ...userToUpdate,
+      ...createUserDto,
+      address: {
+        ...userToUpdate.address,
+        ...createUserDto.address,
+      },
+    };
+
+    if (createUserDto.address && createUserDto.address.type) {
+      updatedUser.address.type = createUserDto.address.type;
+    }
+
+    expect(await controller.updateUser(id, createUserDto)).toEqual(updatedUser);
   });
 });
