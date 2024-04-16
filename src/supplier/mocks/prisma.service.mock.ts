f5d59a1f3370b07137bc5d1f2f2eb1f653901b4f
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-16 11:53:12 +0200

    feat : Init unit test for supplier.service.ts

---
+++ src/supplier/mocks/prisma.service.mock.ts
@@ -0,0 +1,6 @@
+export class PrismaServiceMock {
+  findMany = jest.fn();
+  findUnique = jest.fn();
+  create = jest.fn();
+  update = jest.fn();
+}
--- src/supplier/supplier.service.spec.ts
+++ src/supplier/supplier.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SupplierService } from './supplier.service';
 import { PrismaService } from '../database/prisma/prisma.service';
+import { PrismaServiceMock } from './mocks/prisma.service.mock';
 
 describe('SupplierService', () => {
   let service: SupplierService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SupplierService, PrismaService],
+      providers: [
+        SupplierService,
+        {
+          provide: PrismaService,
+          useClass: PrismaServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<SupplierService>(SupplierService);
