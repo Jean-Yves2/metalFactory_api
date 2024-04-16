fc9d3fd85cd73403a62dab062889ee496f7ede80
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-16 18:00:11 +0200

    feat: add unit test for find supplier by id supplier

--- src/supplier/mocks/prisma.service.mock.ts
+++ src/supplier/mocks/prisma.service.mock.ts
@@ -1,10 +1,19 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { supplierMock } from './supplier.mock';
 export class PrismaServiceMock {
   supplier = {
     findMany: jest.fn().mockImplementation(() => {
       return supplierMock;
     }),
-    findUnique: jest.fn(),
+    findUnique: jest.fn().mockImplementation((params) => {
+      const supplier = supplierMock.find(
+        (supplier) => supplier.id === params.where.id,
+      );
+      if (!supplier) {
+        throw new HttpException('Supplier not found', HttpStatus.NOT_FOUND);
+      }
+      return supplier;
+    }),
     create: jest.fn(),
     update: jest.fn(),
   };
--- src/supplier/supplier.service.spec.ts
+++ src/supplier/supplier.service.spec.ts
@@ -3,6 +3,7 @@ import { SupplierService } from './supplier.service';
 import { PrismaService } from '../database/prisma/prisma.service';
 import { PrismaServiceMock } from './mocks/prisma.service.mock';
 import { supplierMock } from './mocks/supplier.mock';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('SupplierService', () => {
   let service: SupplierService;
@@ -28,6 +29,21 @@ describe('SupplierService', () => {
   describe('getAllSuppliers', () => {
     it('should return all suppliers', async () => {
       expect(await service.getAllSuppliers()).toEqual(supplierMock);
+    });
+  });
+
+  describe('getSupplierById', () => {
+    it('should return a supplier by id', async () => {
+      const getSupplier = supplierMock.find((supplier) => supplier.id === 1);
+      expect(await service.getSupplierById(1)).toEqual(getSupplier);
+    });
+
+    it('should throw HttpException when supplier is not found', async () => {
+      const nonExistingSupplierId = 100;
+      const getUserPromise = service.getSupplierById(nonExistingSupplierId);
+      await expect(getUserPromise).rejects.toThrow(
+        new HttpException('Supplier not found', HttpStatus.NOT_FOUND),
+      );
     });
   });
 });
