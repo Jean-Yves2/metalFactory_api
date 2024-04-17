8f622af6c86b7c9b63a9ecfe7907cfc4551a7be0
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-17 10:13:05 +0200

    fix : unit test for find supplier by id supplier

--- src/supplier/mocks/prisma.service.mock.ts
+++ src/supplier/mocks/prisma.service.mock.ts
@@ -1,4 +1,3 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
 import { supplierMock } from './supplier.mock';
 export class PrismaServiceMock {
   supplier = {
@@ -9,9 +8,6 @@ export class PrismaServiceMock {
       const supplier = supplierMock.find(
         (supplier) => supplier.id === params.where.id,
       );
-      if (!supplier) {
-        throw new HttpException('Supplier not found', HttpStatus.NOT_FOUND);
-      }
       return supplier;
     }),
     create: jest.fn(),
