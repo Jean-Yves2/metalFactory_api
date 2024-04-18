f9885f08c57ed19aa310a0e157037d52826795f8
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-18 13:11:17 +0200

    feat: add unit test for create new delivery

--- src/delivery/delivery.controller.spec.ts
+++ src/delivery/delivery.controller.spec.ts
@@ -4,6 +4,8 @@ import { DeliveryService } from './delivery.service';
 import { PrismaService } from '../database/prisma/prisma.service';
 import { DeliveryServiceMock } from './mocks/delivery.service.mock';
 import { deliveryMock } from './mocks/delivery.mock';
+import { DeliveryStatus } from '@prisma/client';
+import { CreateDeliveryDto } from './dto/create-delivery.dto';
 
 describe('DeliveryController', () => {
   let controller: DeliveryController;
@@ -35,6 +37,23 @@ describe('DeliveryController', () => {
     it('should return a delivery by id', async () => {
       const getDelivery = deliveryMock.find((delivery) => delivery.id === 1);
       expect(await controller.findOne(1)).toEqual(getDelivery);
+    });
+  });
+
+  describe('createDelivery', () => {
+    it('should create a delivery', async () => {
+      const newDelivery: CreateDeliveryDto = {
+        orderId: 1,
+        deliveryCompanyId: 1,
+        distance: 201,
+        weight: 77.77,
+        cost: 50,
+        VATRate: 92,
+        deliveryStatus: DeliveryStatus.DELIVERED,
+      };
+      expect(await controller.create(newDelivery)).toEqual(
+        'Delivery created successfully',
+      );
     });
   });
 });
