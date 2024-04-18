dd720f10921d28952934145b1c8a830316ff5fcf
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-18 10:26:10 +0200

    fix  : all spec providers

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { PrismaService } from '../database/prisma/prisma.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +12,13 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        JwtService,
+        ConfigService,
+        AuthService,
+        UserService,
+        PrismaService,
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/services/faker/faker.controller.spec.ts
+++ src/services/faker/faker.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FakerController } from './faker.controller';
+import { FakerService } from './faker.service';
+import { PrismaService } from '../../database/prisma/prisma.service';
 
 describe('FakerController', () => {
   let controller: FakerController;
@@ -7,6 +9,7 @@ describe('FakerController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FakerController],
+      providers: [FakerService, PrismaService],
     }).compile();
 
     controller = module.get<FakerController>(FakerController);
