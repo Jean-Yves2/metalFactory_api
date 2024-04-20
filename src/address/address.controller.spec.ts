b25fe1bf59c2153e9dbf37018f96885782a5cd4b
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-21 00:20:32 +0200

    feat  :  add unit test deleteAddress on  address

--- src/address/address.controller.spec.ts
+++ src/address/address.controller.spec.ts
@@ -47,4 +47,51 @@ describe('AddressController', () => {
       expect(result).toEqual(address);
     });
   });
+
+  describe('createAddress', () => {
+    it('should create an address', async () => {
+      const newAddress = {
+        street: 'Street 4',
+        postalCode: '12345',
+        city: 'City 4',
+        country: 'Country 4',
+        distanceToWarehouse: 20,
+      };
+      const result = await controller.create(newAddress);
+      expect(result).toEqual(newAddress);
+    });
+  });
+
+  describe('updateAddress', () => {
+    it('should update an address', async () => {
+      const id = 1;
+      const updatedAddress = {
+        street: 'Street 1 Updated',
+        postalCode: '12345',
+        city: 'City 1 Updated 999',
+        country: 'Country 1 Updated',
+        distanceToWarehouse: 30,
+      };
+      const result = await controller.update(id, updatedAddress);
+      expect(result).toEqual(updatedAddress);
+    });
+  });
+
+  describe('deleteAddress', () => {
+    it('should delete an address', async () => {
+      const id = 1;
+      const result = await controller.remove(id);
+      expect(result).toEqual({
+        id: id,
+        deletedAt: expect.any(Date),
+        city: 'City 1',
+        country: 'Country 1',
+        createdAt: new Date('2024-04-18T09:05:38.780Z'),
+        distanceToWarehouse: expect.any(Object), //* ⚠️we need to check the type ⚠️ **/
+        postalCode: '12345',
+        street: 'Street 1',
+        updatedAt: new Date('2024-04-18T09:05:38.780Z'),
+      });
+    });
+  });
 });
--- src/address/mocks/address.service.mock.ts
+++ src/address/mocks/address.service.mock.ts
@@ -1,3 +1,4 @@
+import { Decimal } from '@prisma/client/runtime/library';
 import { addressMock } from './address.mock';
 
 export class AddressServiceMock {
@@ -13,6 +14,28 @@ export class AddressServiceMock {
     );
     return address;
   });
-  createAddress = jest.fn();
-  updateAddress = jest.fn();
+  createAddress = jest.fn().mockImplementation((address) => {
+    addressMock.push(address);
+    return address;
+  });
+  updateAddress = jest.fn().mockImplementation((id: number, address) => {
+    const index = addressMock.findIndex(
+      (address) => address.id === id && address.deletedAt === null,
+    );
+    addressMock[index] = address;
+    return address;
+  });
+  softDelete = jest.fn().mockImplementation((id: number) => {
+    const index = addressMock.findIndex(
+      (address) => address.id === id && address.deletedAt === null,
+    );
+
+    addressMock[index].deletedAt = new Date();
+    addressMock[index].distanceToWarehouse = new Decimal(
+      addressMock[index].distanceToWarehouse,
+    );
+
+    const deletedAddress = { ...addressMock[index] };
+    return deletedAddress;
+  });
 }
