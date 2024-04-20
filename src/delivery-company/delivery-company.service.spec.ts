0e2ac756acd750af7f3d7f4df6eac73f9227b578
Jean-Yves2 <sarakajean1@gmail.com>
2024-04-20 17:12:48 +0200

    feat  :  add unit test findMany on  delivery compagny

--- src/delivery-company/delivery-company.service.spec.ts
+++ src/delivery-company/delivery-company.service.spec.ts
@@ -1,13 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DeliveryCompanyService } from './delivery-company.service';
 import { PrismaService } from '../database/prisma/prisma.service';
+import { DeliveryCompanyPrismaMock } from './mocks/prisma.service.mock';
+import { deliveryCompanyMock } from './mocks/delivery-company.mock';
 
 describe('DeliveryCompanyService', () => {
   let service: DeliveryCompanyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DeliveryCompanyService, PrismaService],
+      providers: [
+        DeliveryCompanyService,
+        {
+          provide: PrismaService,
+          useClass: DeliveryCompanyPrismaMock,
+        },
+      ],
     }).compile();
 
     service = module.get<DeliveryCompanyService>(DeliveryCompanyService);
@@ -15,5 +23,29 @@ describe('DeliveryCompanyService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllDeliveryCompanies', () => {
+    it('should get all delivery companies', async () => {
+      const allDeliveryCompanies = deliveryCompanyMock.filter(
+        (deliveryCompany) => deliveryCompany.deletedAt === null,
+      );
+      const deliveryCompanies = await service.getAllDeliveryCompanies();
+      expect(deliveryCompanies).toEqual(allDeliveryCompanies);
+    });
+
+    it('should throw an error when there is an internal error', async () => {
+      jest
+        .spyOn(service['prismaService'].deliveryCompany, 'findMany')
+        .mockRejectedValue(new Error('Internal Server Error'));
+      try {
+        await service.getAllDeliveryCompanies();
+      } catch (error) {
+        if (error instanceof Error) {
+          expect(error).toBeInstanceOf(Error);
+          expect(error.message).toEqual('Error getting all delivery companies');
+        }
+      }
+    });
   });
 });
--- src/delivery-company/mocks/prisma.service.mock.ts
+++ src/delivery-company/mocks/prisma.service.mock.ts
@@ -1,6 +1,13 @@
+import { deliveryCompanyMock } from './delivery-company.mock';
+
 export class DeliveryCompanyPrismaMock {
   deliveryCompany = {
-    findMany: jest.fn(),
+    findMany: jest.fn().mockImplementation(() => {
+      const allDeliveryCompanies = deliveryCompanyMock.filter(
+        (deliveryCompany) => deliveryCompany.deletedAt === null,
+      );
+      return allDeliveryCompanies;
+    }),
     findUnique: jest.fn(),
     create: jest.fn(),
     update: jest.fn(),
