4ae43a36c8d083ae7732dfe8117f7b7026f301c7
cabrelyvans <dhscabrel@gmail.com>
2024-04-23 13:05:48 +0200

    feat: add unit test "createStock" for stock service

--- src/stock/stock.service.spec.ts
+++ src/stock/stock.service.spec.ts
@@ -46,4 +46,23 @@ describe('StockService', () => {
       );
     });
   });
+
+  describe('createStock', () => {
+    it('should create a stock', async () => {
+      const createStock = {
+        id: 1,
+        warehouseId: 1,
+        productId: 1,
+        quantity: 100,
+        minThreshold: 10,
+        lastUpdated: new Date('2024-04-18T09:05:38.780Z'),
+        createdAt: new Date('2024-04-18T09:05:38.780Z'),
+        updatedAt: new Date('2024-04-18T09:05:38.780Z'),
+        deletedAt: null,
+      };
+      expect(await service.createStock(createStock)).toEqual(
+        'Stock created successfully',
+      );
+    });
+  });
 });
