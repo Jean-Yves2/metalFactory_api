f65204123b6d4b3e93dcfa21e6a14aacb87990bf
cabrelyvans <dhscabrel@gmail.com>
2024-04-24 13:15:01 +0200

    feat: add unit test "findAll" for Warehouse controller

--- src/warehouse/warehouse.controller.spec.ts
+++ src/warehouse/warehouse.controller.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WarehouseController } from './warehouse.controller';
 import { WarehouseService } from './warehouse.service';
-import { PrismaService } from '../database/prisma/prisma.service';
-
+//import { PrismaService } from '../database/prisma/prisma.service';
+import { WarehouseServiceMock } from './mocks/warehouse.service.mock';
+import { warehouseMock } from './mocks/warehouse.mock';
 describe('WarehouseController', () => {
   let controller: WarehouseController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WarehouseController],
-      providers: [WarehouseService, PrismaService],
+      providers: [
+        { provide: WarehouseService, useClass: WarehouseServiceMock },
+      ],
     }).compile();
 
     controller = module.get<WarehouseController>(WarehouseController);
@@ -17,5 +20,11 @@ describe('WarehouseController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getAllWarehouses', () => {
+    it('should return all warehouses', async () => {
+      expect(await controller.findAll()).toEqual(warehouseMock);
+    });
   });
 });
