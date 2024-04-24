b9e21ac85abc5e1f0397cbfe9c17fa0fec686fb6
cabrelyvans <dhscabrel@gmail.com>
2024-04-24 15:02:49 +0200

    feat: add unit test "getAllWarehouse" for warehouse service

--- src/warehouse/warehouse.service.spec.ts
+++ src/warehouse/warehouse.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WarehouseService } from './warehouse.service';
 import { PrismaService } from '../database/prisma/prisma.service';
-
+import { PrismaServiceMock } from './mocks/prisma.service.mock';
+import { warehouseMock } from './mocks/warehouse.mock';
 describe('WarehouseService', () => {
   let service: WarehouseService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WarehouseService, PrismaService],
+      providers: [
+        WarehouseService,
+        {
+          provide: PrismaService,
+          useClass: PrismaServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<WarehouseService>(WarehouseService);
@@ -15,5 +22,11 @@ describe('WarehouseService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllWarehouse', () => {
+    it('should return all warehouses', async () => {
+      expect(await service.getAllWarehouses()).toEqual(warehouseMock);
+    });
   });
 });
