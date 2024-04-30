69cd7da95a43180621a78e504d49e95d481736c0
cabrelyvans <dhscabrel@gmail.com>
2024-04-30 16:10:55 +0200

    feat : add unit test for Order services

--- src/order/order.service.spec.ts
+++ src/order/order.service.spec.ts
@@ -1,13 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
 import { PrismaService } from '../database/prisma/prisma.service';
+import { PrismaServiceMock } from './mocks/prisma.service.mock';
+import { orderMock } from './mocks/order.mock';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { OrderStatus } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime/library';
 
 describe('OrderService', () => {
   let service: OrderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderService, PrismaService],
+      providers: [
+        OrderService,
+        {
+          provide: PrismaService,
+          useClass: PrismaServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<OrderService>(OrderService);
@@ -15,5 +26,94 @@ describe('OrderService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAllOrders', () => {
+    it('should return all orders', async () => {
+      expect(await service.getAllOrders()).toEqual(orderMock);
+    });
+  });
+
+  describe('getOrderById', () => {
+    it('should return a order by id', async () => {
+      const getOrder = orderMock.find((order) => order.id === 1);
+      expect(await service.getOrderById(1)).toEqual(getOrder);
+    });
+
+    it('should throw HttpException when order is not found', async () => {
+      const nonExistingOrderId = 100;
+      const getUserPromise = service.getOrderById(nonExistingOrderId);
+      await expect(getUserPromise).rejects.toThrow(
+        new HttpException('Order not found', HttpStatus.NOT_FOUND),
+      );
+    });
+  });
+
+  describe('createOrder', () => {
+    it('should create a order', async () => {
+      const createOrder = {
+        id: 1,
+        customerId: 1,
+        orderDate: new Date(),
+        status: OrderStatus.PENDING,
+        totalExclTax: new Decimal('4882'),
+        totalInclTax: new Decimal('7892'),
+        deliveryAddressId: 1,
+        createdAt: new Date('2024-03-19T11:32:13.557Z'),
+        updatedAt: new Date('2024-03-19T11:32:13.557Z'),
+        deletedAt: null,
+      };
+      expect(await service.createOrder(createOrder)).toEqual(
+        'Order created successfully',
+      );
+    });
+  });
+
+  describe('updateOrder', () => {
+    it('should update a order', async () => {
+      const orderId = 3;
+      const updatedOrder = {
+        customerId: 1,
+        orderDate: new Date(),
+        status: OrderStatus.PENDING,
+        totalExclTax: new Decimal('4882'),
+        totalInclTax: new Decimal('7892'),
+        deliveryAddressId: 1,
+      };
+      expect(await service.updateOrder(orderId, updatedOrder)).toEqual(
+        'Order updated successfully',
+      );
+    });
+
+    it('should return null when order is not found', async () => {
+      const orderId = 100;
+      const updatedOrder = {
+        customerId: 1,
+        orderDate: new Date(),
+        status: OrderStatus.PENDING,
+        totalExclTax: new Decimal('4882'),
+        totalInclTax: new Decimal('7892'),
+        deliveryAddressId: 1,
+      };
+      await expect(service.updateOrder(orderId, updatedOrder)).rejects.toThrow(
+        new HttpException('Order not found', HttpStatus.NOT_FOUND),
+      );
+    });
+  });
+
+  describe('deleteOrder', () => {
+    it('should delete a order', async () => {
+      const orderId = 3;
+      expect(await service.deleteOrder(orderId)).toEqual(
+        'Order deleted successfully',
+      );
+    });
+
+    it('should return null when order is not found', async () => {
+      const orderId = 100;
+      await expect(service.deleteOrder(orderId)).rejects.toThrow(
+        new HttpException('Order not found', HttpStatus.NOT_FOUND),
+      );
+    });
   });
 });
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,11 +1,6 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  NotFoundException,
-} from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma/prisma.service';
+import { CreateOrderDto } from './dto/create-order.dto';
 import { Order } from '@prisma/client';
 
 @Injectable()
@@ -13,96 +8,77 @@ export class OrderService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getAllOrders(): Promise<Order[]> {
-    try {
-      return await this.prismaService.order.findMany({
-        where: { deletedAt: null },
-      });
-    } catch (error) {
-      throw new InternalServerErrorException('Error fetching all orders');
-    }
+    return this.prismaService.order.findMany();
   }
 
-  async getOrderById(id: number): Promise<Order> {
-    try {
-      const order = await this.prismaService.order.findUnique({
-        where: { id, deletedAt: null },
-      });
-      if (!order) {
-        throw new NotFoundException(`Order with ID ${id} not found`);
-      }
-      return order;
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-      throw new InternalServerErrorException(
-        `Error fetching order with id ${id}`,
-      );
+  async getOrderById(id: number) {
+    const order = await this.prismaService.order.findUnique({
+      where: { id: id, deletedAt: null },
+    });
+
+    if (!order) {
+      throw new HttpException('Order not found', HttpStatus.NOT_FOUND);
     }
+
+    return order;
   }
 
-  async createOrder(createOrderDto: CreateOrderDto): Promise<Order> {
-    try {
-      return await this.prismaService.order.create({
-        data: {
-          ...createOrderDto,
-          createdAt: new Date(),
-        },
-      });
-    } catch (error) {
-      throw new InternalServerErrorException('Error creating order');
-    }
+  async createOrder(createOrderDto: CreateOrderDto) {
+    await this.prismaService.order.create({
+      data: {
+        customerId: createOrderDto.customerId,
+        orderDate: new Date(),
+        status: createOrderDto.status,
+        totalExclTax: createOrderDto.totalExclTax,
+        totalInclTax: createOrderDto.totalInclTax,
+        deliveryAddressId: createOrderDto.deliveryAddressId,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+      },
+    });
+    return 'Order created successfully';
   }
 
-  async updateOrder(
-    id: number,
-    updateOrderDto: UpdateOrderDto,
-  ): Promise<Order> {
-    try {
-      const order = await this.prismaService.order.findUnique({
-        where: { id, deletedAt: null },
-      });
-      if (!order) {
-        throw new NotFoundException(`Order with ID ${id} not found`);
-      }
-      return await this.prismaService.order.update({
-        where: { id },
-        data: {
-          ...updateOrderDto,
-          updatedAt: new Date(),
-        },
-      });
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-      throw new InternalServerErrorException(
-        `Error updating order with id ${id}`,
-      );
+  async updateOrder(id: number, createOrderDto: CreateOrderDto) {
+    const order = await this.prismaService.order.findUnique({
+      where: { id: id, deletedAt: null },
+    });
+
+    if (!order) {
+      throw new HttpException('Order not found', HttpStatus.NOT_FOUND);
     }
+
+    await this.prismaService.order.update({
+      where: { id: id },
+      data: {
+        customerId: createOrderDto.customerId,
+        orderDate: new Date(),
+        status: createOrderDto.status,
+        totalExclTax: createOrderDto.totalExclTax,
+        totalInclTax: createOrderDto.totalInclTax,
+        deliveryAddressId: createOrderDto.deliveryAddressId,
+        updatedAt: new Date(),
+      },
+    });
+    return 'Order updated successfully';
   }
 
-  async deleteOrder(id: number): Promise<Order> {
-    try {
-      const order = await this.prismaService.order.findUnique({
-        where: { id, deletedAt: null },
-      });
-      if (!order) {
-        throw new NotFoundException(`Order with ID ${id} not found`);
-      }
-      return await this.prismaService.order.update({
-        where: { id },
-        data: {
-          deletedAt: new Date(),
-        },
-      });
-    } catch (error) {
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-      throw new InternalServerErrorException(
-        `Error deleting order with id ${id}`,
-      );
+  async deleteOrder(id: number) {
+    const order = await this.prismaService.order.findUnique({
+      where: { id: id, deletedAt: null },
+    });
+
+    if (!order) {
+      throw new HttpException('Order not found', HttpStatus.NOT_FOUND);
     }
+
+    await this.prismaService.order.update({
+      where: { id: id },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+    return 'Order deleted successfully';
   }
 }
